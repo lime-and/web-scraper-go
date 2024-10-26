c452012511bad8671c10bcec29f098dd4e78de87
lime <michaelliman21@gmail.com>
2024-10-27 08:27:29 +0900

    main update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # web-scraper-go
-web scraper in go using colly
+Golang でウェブスクレイピングを行う
 
 ## Usage
 
 ```bash
 go run scraper.go
 ```
+
+実装結果
+![result](image.png)
--- scraper.go
+++ scraper.go
@@ -1,41 +1,86 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
 	"log"
+	"os"
 
-	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly"
 )
 
+// initialize a data structure to keep the scraped data
+type Product struct {
+	Url, Image, Name, Price string
+}
+
 func main() {
-	c := colly.NewCollector()
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+
+	// instantiate a new collector object
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
+
+	// initialize the slice of structs that will contain the scraped data
+	var products []Product
+
+	// OnHTML callback
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+
+		// initialize a new Product instance
+		product := Product{}
+
+		// scrape the target data
+		product.Url = e.ChildAttr("a", "href")
+		product.Image = e.ChildAttr("img", "src")
+		product.Name = e.ChildText(".product-name")
+		product.Price = e.ChildText(".price")
+
+		// add the product instance with scraped data to the list of products
+		products = append(products, product)
+
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL)
-	})
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
-		fmt.Println("First column of a table row:", e.Text)
-	})
-
-	c.OnXML("//h1", func(e *colly.XMLElement) {
-		fmt.Println(e.Text)
-	})
-
+	// store the data to a CSV after extraction
+	// store the data to a CSV after extraction
 	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
+
+		// open the CSV file
+		file, err := os.Create("products.csv")
+		if err != nil {
+			log.Fatalln("Failed to create output CSV file", err)
+		}
+		defer file.Close()
+
+		// initialize a file writer with comma as the delimiter
+		writer := csv.NewWriter(file)
+		writer.Comma = ',' // ensure it's using a comma as the delimiter
+
+		// write the CSV headers
+		headers := []string{
+			"Url",
+			"Image",
+			"Name",
+			"Price",
+		}
+		writer.Write(headers)
+
+		// write each product as a CSV row
+		for _, product := range products {
+			// convert a Product to an array of strings
+			record := []string{
+				product.Url,
+				product.Image,
+				product.Name,
+				product.Price,
+			}
+
+			// add a CSV record to the output file
+			writer.Write(record)
+		}
+		writer.Flush() // make sure to flush the data to the file
 	})
 
-	fmt.Println("Hello, World!")
+	// open the target URL
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
+
 }
